26639a74370e22f3f13929f333a7196d28a1f723
Gopal Agarwal <gopal.agarwal3@gmail.com>
2020-12-05 20:34:47 +0530

    first custom passage formatter implementation

---
+++ src/main/java/com/offset/elasticsearch/plugin/OffsetHighlighter.java
@@ -0,0 +1,25 @@
+package com.offset.elasticsearch.plugin;
+
+import org.apache.lucene.search.highlight.Encoder;
+import org.apache.lucene.search.uhighlight.CustomUnifiedHighlighter;
+import org.apache.lucene.search.uhighlight.PassageFormatter;
+import org.elasticsearch.index.query.QueryShardContext;
+import org.elasticsearch.search.fetch.FetchSubPhase.HitContext;
+import org.elasticsearch.search.fetch.subphase.highlight.UnifiedHighlighter;
+import org.elasticsearch.search.fetch.subphase.highlight.SearchContextHighlight;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OffsetHighlighter extends UnifiedHighlighter {
+
+    public static final String NAME = "annotated";
+
+    @Override
+    protected PassageFormatter getPassageFormatter(HitContext hitContext,SearchContextHighlight.Field field, Encoder encoder) {
+        // Retrieve the annotations from the hitContext
+        return new OffsetPassageFormatter();
+    }
+
+}
